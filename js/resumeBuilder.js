3d87db1a6a2b0642b14d5bbec62d9509da212ed4
David Mallard <correspondence@davidmallard.id.au>
2017-05-29 11:24:08 +1000

    Add a format function to handle inserting content into templates

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,40 +16,30 @@ var bio = {
 		'content strategy'
 	],
 	biopic: 'images/tobby.png',
+	format: function(template, content) {
+		var output = template.replace('%data%', content);
+		return output;
+	},
 	display: function() {
-		var formattedName = HTMLheaderName.replace("%data%", bio.name);
-		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-		var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-		var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-		var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-		var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-		var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-		var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-		var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
-		var formattedSkills0 = HTMLskills.replace("%data%", bio.skills[0]);
-		var formattedSkills1 = HTMLskills.replace("%data%", bio.skills[1]);
-		var formattedSkills2 = HTMLskills.replace("%data%", bio.skills[2]);
-		var formattedSkills3 = HTMLskills.replace("%data%", bio.skills[3]);
-
-		$("#header").prepend(formattedRole);
-		$("#header").prepend(formattedName);
-		$("#topContacts").append(formattedMobile);
-		$("#topContacts").append(formattedEmail);
-		$("#topContacts").append(formattedGithub);
-		$("#topContacts").append(formattedTwitter);
-		$("#topContacts").append(formattedLocation);
-		$("#footerContacts").append(formattedMobile);
-		$("#footerContacts").append(formattedEmail);
-		$("#footerContacts").append(formattedGithub);
-		$("#footerContacts").append(formattedTwitter);
-		$("#footerContacts").append(formattedLocation);
-		$("#header").append(formattedBiopic);
-		$("#header").append(formattedWelcomeMessage);
+		$("#header").prepend(bio.format(HTMLheaderRole, bio.role));
+		$("#header").prepend(bio.format(HTMLheaderName, bio.name));
+		$("#topContacts").append(bio.format(HTMLmobile, bio.contacts.mobile));
+		$("#topContacts").append(bio.format(HTMLemail, bio.contacts.email));
+		$("#topContacts").append(bio.format(HTMLgithub, bio.contacts.github));
+		$("#topContacts").append(bio.format(HTMLtwitter, bio.contacts.twitter));
+		$("#topContacts").append(bio.format(HTMLlocation, bio.contacts.location));
+		$("#footerContacts").append(bio.format(HTMLmobile, bio.contacts.mobile));
+		$("#footerContacts").append(bio.format(HTMLemail, bio.contacts.email));
+		$("#footerContacts").append(bio.format(HTMLgithub, bio.contacts.github));
+		$("#footerContacts").append(bio.format(HTMLtwitter, bio.contacts.twitter));
+		$("#footerContacts").append(bio.format(HTMLlocation, bio.contacts.location));
+		$("#header").append(bio.format(HTMLbioPic, bio.biopic));
+		$("#header").append(bio.format(HTMLwelcomeMsg, bio.welcomeMessage));
 		$("#header").append(HTMLskillsStart);
-		$("#skills").append(formattedSkills0);
-		$("#skills").append(formattedSkills1);
-		$("#skills").append(formattedSkills2);
-		$("#skills").append(formattedSkills3);
+		$("#skills").append(bio.format(HTMLskills, bio.skills[0]));
+		$("#skills").append(bio.format(HTMLskills, bio.skills[1]));
+		$("#skills").append(bio.format(HTMLskills, bio.skills[2]));
+		$("#skills").append(bio.format(HTMLskills, bio.skills[3]));
 	}
 };
 
