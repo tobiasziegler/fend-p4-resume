19a9ceaa13597db526b4833e8d7b3166699cdb2b
David Mallard <correspondence@davidmallard.id.au>
2017-05-29 11:01:41 +1000

    Create the bio object and add all of its content

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	name: 'David Mallard',
+	role: 'Front-End Web Developer',
+	contacts: {
+		mobile: '0432 881 448',
+		email: 'correspondence@davidmallard.id.au',
+		github: 'tobiasziegler',
+		twitter: '@tobiasziegler',
+		location: 'Orange, NSW, Australia'
+	},
+	welcomeMessage: 'I create digital experiences that delight users and meet your organisation\'s goals',
+	skills: [
+		'user experience design',
+		'web development',
+		'research and data analysis',
+		'content strategy'
+	],
+	biopic: 'images/tobby.png',
+	display: function() {
+
+	}
+};
