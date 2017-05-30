ac06fbfda719faa1153dea034ffbdee2b2d384f5
David Mallard <correspondence@davidmallard.id.au>
2017-05-30 11:53:47 +1000

    Create a Grunt task to run JSBeautifier (including CSS and HTML beautifiers)

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,28 @@ module.exports = function(grunt) {
 				// Don't lint the jQuery library
 				'!js/jQuery.js'
 			]
+		},
+		jsbeautifier: {
+			files: [
+				'js/**/*.js',
+				'!js/jQuery.js',
+				'css/**/*.css',
+				'*.html'
+			],
+			options: {
+				js: {
+					indentWithTabs: true
+				},
+				css: {
+					indentWithTabs: true
+				},
+				html: {
+					indentWithTabs: true
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jsbeautifier');
 };
